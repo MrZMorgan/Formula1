51cd84f321d03b9787767be27111a45c8b720eb9
Egor Anchutin <zbit16@gmail.com>
2020-11-28 20:17:35 +0300

    Add method parseDate

--- Formula1/src/ua/com/foxminded/Solution.java
+++ Formula1/src/ua/com/foxminded/Solution.java
@@ -1,7 +1,11 @@
 package ua.com.foxminded;
 
+import java.text.ParseException;
+import java.util.Date;
+import static ua.com.foxminded.util.CustomParser.*;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 //        File start = new File(Main.readLine());
 //        List<String> startLog = Main.readAndCollectLinesFomFile(start);
 
@@ -14,6 +18,5 @@ public class Solution {
 //        for (String s : abbreviationsList) {
 //            System.out.println(s);
 //        }
-
     }
 }
--- Formula1/src/ua/com/foxminded/util/CustomParser.java
+++ Formula1/src/ua/com/foxminded/util/CustomParser.java
@@ -1,5 +1,8 @@
 package ua.com.foxminded.util;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,6 +11,8 @@ public class CustomParser {
     public static final Pattern FULL_NAME_PATTERN = Pattern.compile("[A-Z][a-z]+ [A-Z][a-z]+");
     public static final Pattern TEAM_NAME_PATTERN = Pattern.compile("([A-Z]+ ){1,}[A-Z]+");
     public static final Pattern DATE_TIME_PATTERN = Pattern.compile("(\\d|\\B|-|\\.|_|:)+");
+    private static final SimpleDateFormat SIMPLE_DATE_FORMAT
+            = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss.SSS");
 
     public static String parseLine(Pattern pattern, String line) {
         StringBuilder builder = new StringBuilder();
@@ -18,5 +23,9 @@ public class CustomParser {
         }
 
         return builder.toString();
+    }
+
+    public static Date parseDate(String stringDateRepresentation) throws ParseException {
+        return SIMPLE_DATE_FORMAT.parse(stringDateRepresentation);
     }
 }
