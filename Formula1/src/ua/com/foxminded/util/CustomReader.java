df3d3b212cd56779f2eeb5a2567ca02cb1a6a26b
Egor Anchutin <zbit16@gmail.com>
2020-11-29 17:08:35 +0300

    Add sorting to CustomReader

--- Formula1/src/ua/com/foxminded/util/CustomReader.java
+++ Formula1/src/ua/com/foxminded/util/CustomReader.java
@@ -1,18 +1,14 @@
 package ua.com.foxminded.util;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class CustomReader {
     public static List<String> readAndCollectLinesFomFile(String filename) {
@@ -22,6 +18,7 @@ public class CustomReader {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        Collections.sort(strings);
         return strings;
     }
     public static String readLine() {
