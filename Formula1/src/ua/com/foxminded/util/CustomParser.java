efa5bd08d1752bc0986ca0de7e31aa37faf7f51f
Egor Anchutin <zbit16@gmail.com>
2020-11-29 18:47:01 +0300

    Add test for parseLine Method

--- Formula1/src/ua/com/foxminded/util/CustomParser.java
+++ Formula1/src/ua/com/foxminded/util/CustomParser.java
@@ -2,7 +2,6 @@ package ua.com.foxminded.util;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
--- Formula1/test/ua/com/foxminded/util/CustomParserTest.java
+++ Formula1/test/ua/com/foxminded/util/CustomParserTest.java
@@ -1,24 +1,36 @@
 package ua.com.foxminded.util;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static ua.com.foxminded.util.CustomParser.*;
 
 import java.text.ParseException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomParserTest {
+    private static final String inputStringForParseLine = "DRR_Daniel Ricciardo_RED BULL RACING TAG HEUER";
+    private static final String expectedTeamName = "RED BULL RACING TAG HEUER";
+    private static final String expectedAbbreviation = "DRR";
+    private static final String expectedFullName = "Daniel Ricciardo";
+
+    private static final String inputStringForParseDate = "SVF2018-05-24_12:02:58.917";
+    private static final long expectedParseResult = 1527152578917L;
 
     @Test
-    void parseLine() {
+    void shouldParseLine() {
+        String actualTeamName = parseLine(TEAM_NAME, inputStringForParseLine);
+        String actualAbbreviation = parseLine(RACER_ABBREVIATION, inputStringForParseLine);
+        String actualFullName = parseLine(FULL_NAME, inputStringForParseLine);
 
+        assertEquals(expectedTeamName, actualTeamName);
+        assertEquals(expectedAbbreviation, actualAbbreviation);
+        assertEquals(expectedFullName, actualFullName);
     }
 
     @Test
     void shouldParseDate() throws ParseException {
-        String inputString = "SVF2018-05-24_12:02:58.917";
-
-        long expectedParseResult = 1527152578917L;
-        long actualParseResult = CustomParser.parseDate(inputString);
+        long actualParseResult = CustomParser.parseDate(inputStringForParseDate);
 
         assertEquals(expectedParseResult, actualParseResult);
     }
